0f53da3f72227187d17eca6e7060d561a6636629
Lubna-badi <bordiwala.lubna@ymail.com>
2026-04-15 16:45:42 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-123"
-    key = "main"
+    bucket = "my-app-buckey-test-111"
+    key = "master"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
